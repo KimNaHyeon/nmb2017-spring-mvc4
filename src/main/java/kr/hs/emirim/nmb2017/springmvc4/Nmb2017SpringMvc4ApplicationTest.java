87b8f4baa79e4727742592003b5ee6193e811011
KimNaHyeon <nah990722@e-mirim.hs.kr>
2017-09-12 10:45:33 +0900

    프로젝트시작

---
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/Nmb2017SpringMvc4ApplicationTest.java
@@ -0,0 +1,10 @@
+package kr.hs.emirim.nmb2017.springmvc4;
+
+public class Nmb2017SpringMvc4ApplicationTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
